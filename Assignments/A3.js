454067ce163a3c3adf8f1746ee9f3e8d860bdf28
Alex <73742087+Alexofthefoot@users.noreply.github.com>
2024-03-15 14:56:31 -0700

    starting from 3D cube example code

--- Assignments/A3.js
+++ Assignments/A3.js
@@ -1,18 +1,34 @@
+// LightedCube.js (c) 2012 matsuda
 // Vertex shader program
-var VSHADER_SOURCE =
-  'attribute vec2 coordinates;\n' +
+var VSHADER_SOURCE = 
+  'attribute vec4 a_Position;\n' + 
+  'attribute vec4 a_Color;\n' + 
+  'attribute vec4 a_Normal;\n' +        // Normal
+  'uniform mat4 u_MvpMatrix;\n' +
+  'uniform vec3 u_LightColor;\n' +     // Light color
+  'uniform vec3 u_LightDirection;\n' + // Light direction (in the world coordinate, normalized)
+  'varying vec4 v_Color;\n' +
   'void main() {\n' +
-	'  gl_Position = vec4(coordinates,0.0, 200.0);\n' +
+  '  gl_Position = u_MvpMatrix * a_Position ;\n' +
+  // Make the length of the normal 1.0
+  '  vec3 normal = normalize(a_Normal.xyz);\n' +
+  // Dot product of the light direction and the orientation of a surface (the normal)
+  '  float nDotL = max(dot(u_LightDirection, normal), 0.0);\n' +
+  // Calculate the color due to diffuse reflection
+  '  vec3 diffuse = u_LightColor * a_Color.rgb * nDotL;\n' +
+  '  v_Color = vec4(diffuse, a_Color.a);\n' +
   '}\n';
-   
+
 // Fragment shader program
-var FSHADER_SOURCE =
+var FSHADER_SOURCE = 
+  '#ifdef GL_ES\n' +
   'precision mediump float;\n' +
-  'uniform vec4 u_FragColor;\n' +  // uniform variable
+  '#endif\n' +
+  'varying vec4 v_Color;\n' +
   'void main() {\n' +
-  '  gl_FragColor = u_FragColor;\n' +
+  '  gl_FragColor = v_Color;\n' +
   '}\n';
-  
+
 function main() {
   // Retrieve <canvas> element
   var canvas = document.getElementById('webgl');
@@ -30,89 +46,135 @@ function main() {
     return;
   }
 
-  // Specify the color for clearing <canvas>
+  // Set the vertex coordinates, the color and the normal
+  var n = initVertexBuffers(gl);
+  if (n < 0) {
+    console.log('Failed to set the vertex information');
+    return;
+  }
+
+  // Set the clear color and enable the depth test
   gl.clearColor(0, 0, 0, 1);
-  // Enable the depth test
   gl.enable(gl.DEPTH_TEST);
 
-  // Clear <canvas> and the depth buffer
+  // Get the storage locations of uniform variables and so on
+  var u_MvpMatrix = gl.getUniformLocation(gl.program, 'u_MvpMatrix');
+  var u_LightColor = gl.getUniformLocation(gl.program, 'u_LightColor');
+  var u_LightDirection = gl.getUniformLocation(gl.program, 'u_LightDirection');
+  if (!u_MvpMatrix || !u_LightColor || !u_LightDirection) { 
+    console.log('Failed to get the storage location');
+    return;
+  }
+
+  // Set the light color (white)
+  gl.uniform3f(u_LightColor, 1.0, 1.0, 1.0);
+  // Set the light direction (in the world coordinate)
+  var lightDirection = new Vector3([0.5, 3.0, 4.0]);
+  lightDirection.normalize();     // Normalize
+  gl.uniform3fv(u_LightDirection, lightDirection.elements);
+
+  // Calculate the view projection matrix
+  var mvpMatrix = new Matrix4();    // Model view projection matrix
+  mvpMatrix.setPerspective(30, canvas.width/canvas.height, 1, 100);
+  mvpMatrix.lookAt(3, 3, 7, 0, 0, 0, 0, 1, 0);
+  // Pass the model view projection matrix to the variable u_MvpMatrix
+  gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix.elements);
+
+  // Clear color and depth buffer
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-  
-  var disk_radius = 150; // the radius of disk
 
-   // Start drawing
-  var tick = function() {
-	  
-	  // Clear <canvas> and depth buffer
-	    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-	  
-	  draw_circle(gl, 0, 0, disk_radius, [1.0, 1.0, 1.0, 1]);	
-    requestAnimationFrame(tick, canvas); // Request that the browser calls tick
-  };
-  tick(); 
+  gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);   // Draw the cube
 }
 
-// generate a random floating point value between 0 and max
-function getRandom(max) {
-	return Math.random() * Math.floor(max);
+function initVertexBuffers(gl) {
+  // Create a cube
+  //    v6----- v5
+  //   /|      /|
+  //  v1------v0|
+  //  | |     | |
+  //  | |v7---|-|v4
+  //  |/      |/
+  //  v2------v3
+  var vertices = new Float32Array([   // Coordinates
+     1.0, 1.0, 1.0,  -1.0, 1.0, 1.0,  -1.0,-1.0, 1.0,   1.0,-1.0, 1.0, // v0-v1-v2-v3 front
+     1.0, 1.0, 1.0,   1.0,-1.0, 1.0,   1.0,-1.0,-1.0,   1.0, 1.0,-1.0, // v0-v3-v4-v5 right
+     1.0, 1.0, 1.0,   1.0, 1.0,-1.0,  -1.0, 1.0,-1.0,  -1.0, 1.0, 1.0, // v0-v5-v6-v1 up
+    -1.0, 1.0, 1.0,  -1.0, 1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0,-1.0, 1.0, // v1-v6-v7-v2 left
+    -1.0,-1.0,-1.0,   1.0,-1.0,-1.0,   1.0,-1.0, 1.0,  -1.0,-1.0, 1.0, // v7-v4-v3-v2 down
+     1.0,-1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0, 1.0,-1.0,   1.0, 1.0,-1.0  // v4-v7-v6-v5 back
+  ]);
+
+
+  var colors = new Float32Array([    // Colors
+    1, 0, 0,   1, 0, 0,   1, 0, 0,  1, 0, 0,     // v0-v1-v2-v3 front
+    1, 0, 0,   1, 0, 0,   1, 0, 0,  1, 0, 0,     // v0-v3-v4-v5 right
+    1, 0, 0,   1, 0, 0,   1, 0, 0,  1, 0, 0,     // v0-v5-v6-v1 up
+    1, 0, 0,   1, 0, 0,   1, 0, 0,  1, 0, 0,     // v1-v6-v7-v2 left
+    1, 0, 0,   1, 0, 0,   1, 0, 0,  1, 0, 0,     // v7-v4-v3-v2 down
+    1, 0, 0,   1, 0, 0,   1, 0, 0,  1, 0, 0　    // v4-v7-v6-v5 back
+ ]);
+
+
+  var normals = new Float32Array([    // Normal
+    0.0, 0.0, 1.0,   0.0, 0.0, 1.0,   0.0, 0.0, 1.0,   0.0, 0.0, 1.0,  // v0-v1-v2-v3 front
+    1.0, 0.0, 0.0,   1.0, 0.0, 0.0,   1.0, 0.0, 0.0,   1.0, 0.0, 0.0,  // v0-v3-v4-v5 right
+    0.0, 1.0, 0.0,   0.0, 1.0, 0.0,   0.0, 1.0, 0.0,   0.0, 1.0, 0.0,  // v0-v5-v6-v1 up
+   -1.0, 0.0, 0.0,  -1.0, 0.0, 0.0,  -1.0, 0.0, 0.0,  -1.0, 0.0, 0.0,  // v1-v6-v7-v2 left
+    0.0,-1.0, 0.0,   0.0,-1.0, 0.0,   0.0,-1.0, 0.0,   0.0,-1.0, 0.0,  // v7-v4-v3-v2 down
+    0.0, 0.0,-1.0,   0.0, 0.0,-1.0,   0.0, 0.0,-1.0,   0.0, 0.0,-1.0   // v4-v7-v6-v5 back
+  ]);
+
+
+  // Indices of the vertices
+  var indices = new Uint8Array([
+     0, 1, 2,   0, 2, 3,    // front
+     4, 5, 6,   4, 6, 7,    // right
+     8, 9,10,   8,10,11,    // up
+    12,13,14,  12,14,15,    // left
+    16,17,18,  16,18,19,    // down
+    20,21,22,  20,22,23     // back
+ ]);
+
+
+  // Write the vertex property to buffers (coordinates, colors and normals)
+  if (!initArrayBuffer(gl, 'a_Position', vertices, 3, gl.FLOAT)) return -1;
+  if (!initArrayBuffer(gl, 'a_Color', colors, 3, gl.FLOAT)) return -1;
+  if (!initArrayBuffer(gl, 'a_Normal', normals, 3, gl.FLOAT)) return -1;
+
+  // Write the indices to the buffer object
+  var indexBuffer = gl.createBuffer();
+  if (!indexBuffer) {
+    console.log('Failed to create the buffer object');
+    return false;
+  }
+
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+
+  return indices.length;
 }
 
-function draw_circle(gl, x, y, r, color){
-		/**
-		* (x, y): center of the circle 
-		* r: radius
-		* color: [R,G,B,A] ranging from 0.0 to 1.0
-		*/
+function initArrayBuffer (gl, attribute, data, num, type) {
+  // Create a buffer object
+  var buffer = gl.createBuffer();
+  if (!buffer) {
+    console.log('Failed to create the buffer object');
+    return false;
+  }
+  // Write date into the buffer object
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+  // Assign the buffer object to the attribute variable
+  var a_attribute = gl.getAttribLocation(gl.program, attribute);
+  if (a_attribute < 0) {
+    console.log('Failed to get the storage location of ' + attribute);
+    return false;
+  }
+  gl.vertexAttribPointer(a_attribute, num, type, false, 0, 0);
+  // Enable the assignment of the buffer object to the attribute variable
+  gl.enableVertexAttribArray(a_attribute);
 
-		//Define the geometry and store it in buffer objects
-		//Represent a circle disk through 360 trangles
-		 n = 3*360; // number of total vertices
-		 var vertices = [];
-		 for (var i = 1; i <= 360; i++) {
-		 	vertices.push(x);
-		 	vertices.push(y);
-		 	vertices.push(x+r*Math.sin(i-1));
-		 	vertices.push(y+r*Math.cos(i-1));
-		 	vertices.push(x+r*Math.sin(i));
-		 	vertices.push(y+r*Math.cos(i));
-		 }
-		    
-		 // Create a new buffer object
-		 var vertex_buffer = gl.createBuffer();
-		
-		 // Bind an empty array buffer to it
-		 gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
-		 
-		 // Pass the vertices data to the buffer
-		 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-		 
-		 //Get the attribute location
-		 var coord = gl.getAttribLocation(gl.program, "coordinates");
-		 
-		 if(coord < 0) {
-			console.log('Failed to get the storage location of coord');
-			return -1;
-		}
-		
-		// Get the storage location of u_FragColor
-		var u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
-		if (!u_FragColor) {
-			console.log('Failed to get the storage location of u_FragColor');
-			return;
-		}
-  
-		 //point an attribute to the currently bound VBO
-		 gl.vertexAttribPointer(coord, 2, gl.FLOAT, false, 0, 0);
-		 //Enable the attribute
-		 gl.enableVertexAttribArray(coord); 
-		 
-		 // Pass the color of a point to u_FragColor variable
-		 var rgba = color;
-		 gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
-		 
-		// Draw the triangles
-		 gl.drawArrays(gl.TRIANGLE_STRIP, 0, n);
-		 
-		// Unbind the buffer
-		 gl.bindBuffer(gl.ARRAY_BUFFER, null);
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+  return true;
 }
